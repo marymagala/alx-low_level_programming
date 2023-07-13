5b9732196a5e59376c1fe9e7aeb35c8777a01132
marymagala <marymagalam@gmail.com>
2023-07-13 15:50:13 -0700

    hello

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+/**
+ * main - a program that prints a line using function printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
