61eb8c6e1eb9ed4939cde759391e44dce9ef15ac
marymagala <marymagalam@gmail.com>
2023-07-17 02:18:35 -0700

    shell

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,8 +5,10 @@
  */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("zize of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
+}
